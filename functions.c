df3486e1337eba30cdffb7f4f323fb61cf7a2cba
angelharper <angelharper9203@gmail.com>
2023-05-28 16:22:57 -0700

    task done by angel

--- functions.c
+++ functions.c
@@ -1,31 +1,31 @@
 #include "main.h"
-/******************************* PRINT CHAR *********************************/
+/************************* PRINT CHAR *************************/
 /**
  * print_char - Prints a char
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags: Calculates active flags
- * @width: Width
+ * @flags:  Calculates active flags
+ * @width: Width (I
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_char(va_list type, char buffer[],
+int print_char(va_list types, char buffer[],
 int flags, int width, int precision, int size)
 {
-	char c = va_arg(types, int);
-	return (handle_write_char(c, buffer, flag, width, prescion, size))
+char c = va_arg(types, int);
+return (handle_write_char(c, buffer, flags, width, precision, size));
 }
-/********************************* PRINT A SYRING ****************************/
+/************************* PRINT A STRING *************************/
 /**
  * print_string - Prints a string
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags: Calculates active flags
- * @width: get width
+ * @flags:  Calculates active flags
+ * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
- * Return: number of char printed
+ * Return: Number of chars printed
  */
 int print_string(va_list types, char buffer[],
 int flags, int width, int precision, int size)
@@ -39,32 +39,32 @@ UNUSED(precision);
 UNUSED(size);
 if (str == NULL)
 {
-	str = "(null)";
-	if (precision >= 6)
-		str = "     ";
+str = "(null)";
+if (precision >= 6)
+str = "      ";
 }
 while (str[length] != '\0')
 length++;
-if (precision >= 0 && precisio < length)
-	length = precision;
-	if (width > length)
+if (precision >= 0 && precision < length)
+length = precision;
+if (width > length)
 {
-	if (width & F_MINUS)
-	{
-		write(1, &str[0], length);
-		for (i = width - length: i > 0; i--)
-			write(1, " ", 1);
-		return (width);
-	}
-	else
-	{
-		for (i = width - length; i > 0; i--)
-			write(1, " ",1);
-			write(1, &str[0], length);
-		return (width);
-	}
+if (flags & F_MINUS)
+{
+write(1, &str[0], length);
+for (i = width - length; i > 0; i--)
+write(1, " ", 1);
+return (width);
 }
-return (write(1, str, length);
+else
+{
+for (i = width - length; i > 0; i--)
+write(1, " ", 1);
+write(1, &str[0], length);
+return (width);
+}
+}
+return (write(1, str, length));
 }
 /************************* PRINT PERCENT SIGN *************************/
 /**
@@ -166,4 +166,3 @@ count++;
 }
 return (count);
 }
-
