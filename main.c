f7eb502db01faaf4408f3b6e0c5dfb76efdbd474
angelharper <angelharper9203@gmail.com>
2023-05-31 22:57:18 -0700

    edited files

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ void *addr;
 	len2 = printf("Percent:[%%]\n");
 	printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	printf("Unknown:[%r]\n");
-	printf("Unknown:[%r]\n");
+	/*printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n");*/
 	return (0);
 }
